7af84e1f50cd0705150d370887d2297fca5efc7f
Petr Siegl <p.siegl@email.cz>
2019-07-24 07:43:02 +0200

    visible in edit for derived signals

--- client/src/settings/signal-sets/signals/CUD.js
+++ client/src/settings/signal-sets/signals/CUD.js
@@ -196,13 +196,13 @@ export default class CUD extends Component {
         if (isPainless(data.source)) {
             data.settings = {painlessScript: data.painlessScript};
             data.weight_list = null;
-            data.weight_edit = null;
             data.indexed = false;
         } else {
             data.settings = {};
             data.weight_list = data.shownInList ? Number.parseInt(data.weight_list || '0') : null;
-            data.weight_edit = data.shownInEdit ? Number.parseInt(data.weight_edit || '0') : null;
         }
+
+        data.weight_edit = data.shownInEdit ? Number.parseInt(data.weight_edit || '0') : null;
 
         return filterData(data, [
             'cid',
@@ -278,6 +278,7 @@ export default class CUD extends Component {
             }
         }
 
+
         return (
             <Panel title={isEdit ? t('Edit Signal') : t('Create Signal')}>
                 {canDelete &&
@@ -302,11 +303,11 @@ export default class CUD extends Component {
                         :
                         <div className="alert alert-info" role="alert">{t('Choose source first...')}</div>}
 
-                    {isPainless(this.getFormValue('source')) &&
+                    {isPainless(source) &&
                     <TextArea id="painlessScript" label={t('Painless script')}/>
                     }
 
-                    {!isPainless(this.getFormValue('source')) &&
+                    {!isPainless(source) &&
                     <>
                         <CheckBox id="indexed" text={t('Indexed')}/>
 
@@ -316,12 +317,14 @@ export default class CUD extends Component {
                                     help={t('This number determines if in which order the signal is listed when viewing records in the data set. Signals are ordered by weight in ascending order.')}/>
                         }
 
-                        <CheckBox id="shownInEdit" label={t('Record edit')} text={t('Visible in record edit form')}/>
-                        {this.getFormValue('shownInEdit') &&
-                        <InputField id="weight_edit" label={t('Edit weight')}
-                                    help={t('This number determines if in which order the signal is listed when editing records in the data set. Signals are ordered by weight in ascending order.')}/>
-                        }
+
                     </>
+                    }
+
+                    <CheckBox id="shownInEdit" label={t('Record edit')} text={t('Visible in record edit form')}/>
+                    {this.getFormValue('shownInEdit') &&
+                    <InputField id="weight_edit" label={t('Edit weight')}
+                                help={t('This number determines if in which order the signal is listed when editing records in the data set. Signals are ordered by weight in ascending order.')}/>
                     }
 
                     <NamespaceSelect/>
