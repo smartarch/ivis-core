7d00168a35bb3a72f22b3c711921295562cb92c1
David Burian <david.burian@me.com>
2020-01-24 12:42:43 +0000

    Adding @babel/blugin-proposal-optional-chaining

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -34,7 +34,8 @@ module.exports = {
                             plugins: [
                                 ["@babel/plugin-proposal-decorators", { "legacy": true }],
                                 ["@babel/plugin-proposal-class-properties", { "loose" : true }],
-                                "@babel/plugin-proposal-function-bind"
+                                "@babel/plugin-proposal-function-bind",
+                                "@babel/plugin-proposal-optional-chaining"
                             ]
                         }
                     }
