cb5f0a700abe3c8d3eeea234bcf16c4383ad3baa
David Burian <david.burian@me.com>
2020-04-11 22:23:47 +0200

    Code cleanup, removing unnecessary components, default color configuration of timeline

--- client/src/workspaces/ServerAnimationPanel.js
+++ client/src/workspaces/ServerAnimationPanel.js
@@ -5,7 +5,7 @@ import {ServerAnimationContext} from "../ivis/ServerAnimationContext";
 import TestWorkspacePanel
     from "./panels/TestWorkspacePanel";
 import {linear} from "../lib/animation-interpolations";
-import {StopButton, PlayPauseButton, JumpForwardButton, JumpBackwardButton, PlaybackSpeedSlider, Timeline, AnimationTimeline_} from "../lib/media-controls";
+import {StopButton, PlayPauseButton, JumpForwardButton, JumpBackwardButton, PlaybackSpeedSlider, PlaybackTimeline, AnimationTimeline} from "../lib/media-controls";
 
 
 class SampleAnimation extends Component {
@@ -61,69 +61,13 @@ class SampleAnimation extends Component {
                             maxFactor={2.25}
                         />
 
-                        <Timeline
-                            id={0}
-                            length={1000*60*30}
-                            beginTs={0}
-                            endTs={1000*60*60*24*30*6}
-                            position={1000*60*4}
-                            margin={{
-                                left: 20,
-                                right: 20,
-                                top: 50,
-                                bottom: 20
-                            }}
-                            // ticks={{
-                            //     values: [1000*60*60*24*30*3, 1000*60*60*24*30*2],
-                            //     labels: [0, 1000*60*60*24*30*3, 1000*60*60*24*30*6],
-                            // }}
-                        />
 
-                        <Timeline
-                            id={1}
-                            length={1000*60*30}
-                            beginTs={0}
-                            relative
-                            endTs={1000*60*60*24}
-                            position={1000*60*4}
-                            margin={{
-                                left: 20,
-                                right: 20,
-                                top: 50,
-                                bottom: 20
-                            }}
-                        />
-
-                        {/* <SelectableTimeline */}
-                        {/*     height={200} */}
-                        {/*     width={600} */}
-                        {/*     margin={{ */}
-                        {/*         left: 20, */}
-                        {/*         right: 50, */}
-                        {/*         bottom: 20, */}
-                        {/*         top: 10, */}
-                        {/*     }} */}
-                        {/*     relative */}
-                        {/*     beginTs={0} */}
-                        {/*     endTs={1000*60*60*24} */}
-                        {/*     position={1000*60*4} */}
-                        {/* /> */}
-
-                        <AnimationTimeline_
+                        <AnimationTimeline
                             width={600}
                             animConfig={this.animConfig}
                             animStatus={this.animStatus}
                         />
 
-
-                        {/* <Timeline */}
-                        {/*     length={2.5*60*60*1000} */}
-                        {/*     width={"100%"} */}
-                        {/*     bigTickMargin={100} */}
-                        {/*     smallTickMargin={15} */}
-                        {/*     labelFontSize={"89px"} */}
-                        {/*     begin={Date.now()} */}
-                        {/* /> */}
                     </div>
                 </ServerAnimationContext>
             </>
