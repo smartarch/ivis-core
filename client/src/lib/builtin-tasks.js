f9d41ee39ee20b3a17fcecdca5105112ddf5eb00
Petr Siegl <p.siegl@email.cz>
2020-05-05 10:54:05 +0200

    Update refactored builtin tasks loading
    Builtin tasks were stored in user config, which causes huge load and they are not needed all the time. Removed them from config and created REST API.

--- client/src/lib/builtin-tasks.js
+++ client/src/lib/builtin-tasks.js
@@ -1,17 +1,9 @@
 "use strict";
 
-import ivisConfig from "ivisConfig";
+import axios from "axios";
+import {getUrl} from "./urls";
 
-const builtinTasks = ivisConfig.builtinTasks;
-
-export function getBuiltinTasks() {
-    return builtinTasks;
-}
-
-export function getBuiltinTask(taskId) {
-    return builtinTasks.find(task => task.id === taskId);
-}
-
-export function anyBuiltinTask() {
-    return builtinTasks.length !== 0;
+export async function fetchBuiltinTasks() {
+    const data = await axios.get(getUrl(`rest/builtin-tasks`));
+    return data.data;
 }
