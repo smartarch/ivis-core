0f35dee70eada7227db04c4de3548678935b3671
Michal Töpfer <michal.topfer@gmail.com>
2021-03-08 14:35:06 +0100

    Use longtext instead of json for database compatibility

--- server/knex/migrations/20210308000000_add_signal_sets_metadata.js
+++ server/knex/migrations/20210308000000_add_signal_sets_metadata.js
@@ -1,6 +1,8 @@
 exports.up = (knex, Promise) => (async () => {
     await knex.schema.table('signal_sets', table => {
-        table.json('metadata');
+        // this should be JSON, but it doesn't exist in MariaDB running on our server
+        // table.json('metadata');
+        table.text('metadata', 'longtext');
     });
 })();
 
