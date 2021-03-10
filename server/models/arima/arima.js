c251a0499937e7ec27e6889e5e95989b9e324e95
Miroslav Hrabal <mhn@email.cz>
2021-03-10 23:28:13 +0100

    Fix:use relative path when loading arima task

--- server/models/arima/arima.js
+++ server/models/arima/arima.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 const { TaskSource, BuildState, TaskType, subtypesByType} = require("../../../shared/tasks");
 
-const taskCode = fs.readFileSync('models/arima/arima-task.py', 'utf-8');
+const taskPath = path.join(__dirname, 'arima-task.py'); // relative path
+const taskCode = fs.readFileSync(taskPath, 'utf-8');
 const taskParams = require('./arima-params.json');
 
 const arimaTask = {
