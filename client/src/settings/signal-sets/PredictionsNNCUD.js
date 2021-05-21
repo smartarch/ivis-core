52259880660ee01bf072137f80920280a75df19b
Michal Töpfer <michal.topfer@gmail.com>
2021-05-21 13:11:22 +0200

    NN: UI for signal selection using ParamTypes

--- client/src/settings/signal-sets/PredictionsNNCUD.js
+++ client/src/settings/signal-sets/PredictionsNNCUD.js
@@ -14,6 +14,61 @@ import {
     InputField,
     withForm
 } from "../../lib/form";
+import ParamTypes from "../ParamTypes";
+import {SignalType} from "../../../../shared/signals";
+
+const signalsConfigSpec = (id, label, help, sigSet) => ({
+    "id": id,
+    "label": label,
+    "help": help,
+    "type": "fieldset",
+    "cardinality": "1..n",
+    "children": [{
+        "id": "cid",
+        "label": "Signal",
+        "type": "signal",
+        "signalSet": sigSet
+    },{
+        "id": "data_type",
+        "label": "Data Type",
+        "type": "option",
+        "options": [{
+            "key": "auto",
+            "label": "Automatic"
+        },{
+            "key": "numerical",
+            "label": "Numerical"
+        },{
+            "key": "categorical",
+            "label": "Categorical"
+        }]
+    },{
+        "id": "min",
+        "label": "Minimum value",
+        "help": "Leave empty for automatic normalization of the data. Only valid for numerical signals.",
+        "type": "float"
+    },{
+        "id": "max",
+        "label": "Maximum value",
+        "help": "Leave empty for automatic normalization of the data. Only valid for numerical signals.",
+        "type": "float"
+    },{
+        "id": "aggregation",
+        "label": "Aggregation",
+        "help": "Applied only when aggregation interval is specified, valid only for numerical signals.",
+        "type": "option",
+        "options": [{
+            "key": "avg",
+            "label": "Average (default)"
+        },{
+            "key": "min",
+            "label": "Minimum"
+        },{
+            "key": "max",
+            "label": "Maximum"
+        }]
+    }]
+})
 
 @withComponentMixins([
     withTranslation,
@@ -27,13 +82,46 @@ export default class CUD extends Component {
         super(props);
 
         this.state = { };
+
+        // for rendering in ParamTypes, the configSpec of each rendered param must be an array
+        this.tsSigCid_configSpec = [{
+            "id": "tsSigCid",
+            "label": "Timestamp Signal",
+            "type": "signal",
+            "signalType": SignalType.DATE_TIME,
+            "signalSet": props.signalSet.cid,
+        }];
+        this.inputSignals_configSpec = [signalsConfigSpec("inputSignals", "Input Signals", "Signals to use for prediction.", props.signalSet.cid)]
+        this.targetSignals_configSpec = [signalsConfigSpec("targetSignals", "Target Signals", "Signals to predict.", props.signalSet.cid)]
+        this.configSpec = [].concat(
+            this.tsSigCid_configSpec,
+            this.inputSignals_configSpec,
+            this.targetSignals_configSpec,
+        );
+
         this.initForm();
+        this.paramTypes = new ParamTypes(props.t);
     }
 
     componentDidMount() {
+        // prepare default values
+        const defaultValues = {
+            inputSignals: [],
+            targetSignals: [],
+        };
+        if (this.props.signalSet.settings.hasOwnProperty("ts"))
+            defaultValues.tsSigCid = this.props.signalSet.settings.ts;
+
+        // populate default values to the form inputs rendered using the ParamTypes
+        const formValues = {};
+        this.paramTypes.setFields(this.configSpec, defaultValues, formValues);
+
         this.populateFormValues({
-            name: '', // TODO
+            loaded: true, // for determining whether the default values were already loaded
+            name: '', // TODO (MT)
+            ...formValues,
         });
+
     }
 
     async submitHandler(submitAndLeave) {
@@ -42,7 +130,7 @@ export default class CUD extends Component {
         const url = `rest/signal-sets/${this.props.signalSet.id}/predictions/neural_network`;
 
         try {
-            this.disableForm();
+            //this.disableForm(); // TODO (MT)
             this.setFormStatusMessage('info', t('Saving...'));
 
             const submitResult = await this.validateAndSendFormValuesToURL(sendMethod, url);
@@ -51,7 +139,7 @@ export default class CUD extends Component {
                 if (submitAndLeave) {
                     this.navigateToWithFlashMessage(`/settings/signal-sets/${this.props.signalSet.id}/predictions`, 'success', t('Prediction model saved'));
                 } else {
-                    // TODO
+                    // TODO (MT)
                 }
             } else {
                 this.enableForm();
@@ -62,19 +150,69 @@ export default class CUD extends Component {
         }
     }
 
+    /** Remove the param_ attributes from the form state. They are processed
+     *  separately. */
+    filterOutParams(data) {
+        const result = {};
+        for (const attr in data) {
+            if (data.hasOwnProperty(attr)) {
+                if (attr.startsWith('param_'))
+                    continue;
+                result[attr] = data[attr];
+            }
+        }
+        delete result.loaded;
+        return result;
+    }
+
+    submitFormValuesMutator(data) {
+        const paramData = this.paramTypes.getParams(this.configSpec, data);
+        data = this.filterOutParams(data);
+        return {
+            ...data,
+            ...paramData,
+        }
+    }
+
     localValidateFormValues(state) {
-        // TODO
+        // clear the errors for params
+        const paramPrefix = this.paramTypes.getParamPrefix();
+        for (const paramId of state.keys()) {
+            if (paramId.startsWith(paramPrefix)) {
+                state.deleteIn([paramId, 'error']);
+            }
+        }
+        // validate params
+        this.paramTypes.localValidate(this.configSpec, state);
+
+        // TODO (MT): validate other fields
     }
 
     render() {
         const t = this.props.t;
 
-        // TODO
+        // TODO (MT)
+
+        const renderParam = (spec) => {
+            const loaded = this.getFormValue('loaded');
+            if (loaded)
+                return this.paramTypes.render(spec, this)
+        }
+
 
         return (
             <Panel title="Add Neural Network model">
                 <Form stateOwner={this} onSubmitAsync={::this.submitHandler}>
                     <InputField id="name" label={t('Model name')} help={t('Has to be unique among models belonging to this signal set.')} />
+
+                    {renderParam(this.tsSigCid_configSpec)}
+
+                    {renderParam(this.inputSignals_configSpec)}
+
+                    {renderParam(this.targetSignals_configSpec)}
+
+                    <pre>{JSON.stringify(this.getFormValues(), null, 2)}</pre>
+                    <pre>{JSON.stringify(this.props.signalSet, null, 2)}</pre>
 
                     <ButtonRow>
                         <Button type="submit" className="btn-primary" icon="check" label={t('Save and leave')} onClickAsync={async () => await this.submitHandler(true)}/>
--- server/models/predictions-nn.js
+++ server/models/predictions-nn.js
@@ -10,18 +10,38 @@ async function createNNModelTx(tx, context, sigSetId, params) {
     const signalSet = await tx('signal_sets').where('id', sigSetId).first();
     enforce(signalSet, `Signal set ${sigSetId} not found`);
 
+    const tsExists = await tx('signals').where({ set: sigSetId, cid: params.tsSigCid }).first();
+    enforce(tsExists, `Timestamp signal not found in ${sigSetId}`);
+
     const namespace = signalSet.namespace;
 
     let prediction = {
-        name: params.name,
+        name: params.name || '',
         type: PredictionTypes.NN,
         set: sigSetId,
-        ahead_count: 1, // TODO
+        ahead_count: 1, // TODO (MT)
         future_count: 1,
         namespace: namespace
     };
 
-    await predictions.registerPredictionModelTx(tx, context, prediction, [], []);
+    // target signals – signals of the created prediction signal sets
+    // TODO (MT): what to do with aggregated signals
+    const targetSignals = [];
+    for (const sig of params.targetSignals) {
+        const signal = await tx('signals').where('namespace', namespace).where('cid', sig.cid).first();
+
+        targetSignals.push({
+            cid: signal.cid,
+            name: signal.name,
+            description: signal.description,
+            namespace: namespace,
+            type: signal.type,
+            indexed: signal.indexed,
+            weight_list: signal.weight_list
+        });
+    }
+
+    await predictions.registerPredictionModelTx(tx, context, prediction, targetSignals);
 
     return { prediction };
 }
