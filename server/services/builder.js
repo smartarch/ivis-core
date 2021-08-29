28fd2dad2f606fc495f54578d71f5eaf1f9f7081
Petr Siegl <p.siegl@email.cz>
2021-08-29 13:12:19 +0200

    feat: updated packages to latest minor versions
    Includes fixes for breaking changes.

--- server/services/builder.js
+++ server/services/builder.js
@@ -61,6 +61,8 @@ function getWebpackConfig(moduleId) {
                                 plugins: [
                                     ["@babel/plugin-proposal-decorators", { "legacy": true }],
                                     ["@babel/plugin-proposal-class-properties", { "loose" : true }],
+                                    ["@babel/plugin-proposal-private-methods", { "loose": true }],
+                                    ["@babel/plugin-proposal-private-property-in-object", { "loose": true }],
                                     "@babel/plugin-proposal-function-bind"
                                 ]
                             }
