03f5bad2e50667d00ff7d01d66cfa8670db4d048
Michal Töpfer <michal.topfer@gmail.com>
2021-11-15 14:34:14 +0100

    NN: increment the python library version

--- server/lib/tasks/python/ivis/setup.py
+++ server/lib/tasks/python/ivis/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ivis",
-    version="0.0.1",
+    version="0.0.2",
     author='Petr Siegl',
     author_email='p.siegl@email.cz',
     description='python library for tasks in IVIS-CORE project',
